59df531094fd60212fbf0a917014c6f8b55549bc
himynameisfil <nguye208@gmail.com>
2015-02-26 21:30:55 -0500

    Create problem015.py

---
+++ problem015.py
@@ -0,0 +1,18 @@
+# Project-Euler-dot-com
+Solutions to problems done on projecteuler.com
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return n*factorial(n-1)
+def combination(n,k):
+    return int(factorial(n)/(factorial(k)*factorial(n-k)))
+
+print(combination(40,20))
+
+#instead of a graph, you can think of each route as a direction.
+#e.g. the first example is right right down down(RRDD), second is RDRD.
+#The important thing to note is that the number of moves it'll take to get to the corner is fixed
+#along with the number of rights and down's you need to get there
+#So the example problem is equivalent to asking "How many ways can you rearrange a 4 letter sequence with 2 R's and the rest D's?
+#That's just 4 choose 2. and if you change the stuff for the problem at hand, it's 4
