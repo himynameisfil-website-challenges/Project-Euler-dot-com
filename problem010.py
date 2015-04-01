fcb15ccebcdd872c5a5cfbedea5e3b0b451f1ac5
himynameisfil <nguye208@gmail.com>
2015-04-01 19:50:17 -0400

    Update problem010.py

--- problem010.py
+++ problem010.py
@@ -1,7 +1,11 @@
 # Project-Euler-dot-com
-Solutions to problems done on projecteuler.com
+#Solutions to problems done on projecteuler.com
+#Problem: The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+#Find the sum of all the primes below two million.
+
+#This will look at all odd numbers between 3 and two million and add them together under the variable primesum... then add 2(technically it starts as 2 and add the other ones)
 primesum=2
-x=int(input("You want the sum of all primes below.."))
+x=2000000
 for i in range(3,x+1,2):
     for j in range(3,int(round(i**.5+1)),2):
         if i%j==0:
