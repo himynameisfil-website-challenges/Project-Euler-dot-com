b37723a05120c2971e8df2ebe045afc0499b1a3c
himynameisfil <nguye208@gmail.com>
2015-04-05 13:39:07 -0400

    Update problem017.py

--- problem017.py
+++ problem017.py
@@ -1,5 +1,11 @@
-# Project-Euler-dot-com
-Solutions to problems done on projecteuler.com
+##Status: Solved
+##Problem: If the numbers 1 to 5 are written out in words: one, two, three, four, five, then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+##
+##If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
+##
+##
+##NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
+
 numbermap = [0,3,3,5,4,4,3,5,5,4]  #This is a map from our base numbers to the ammount of letters used
 hundred = 7
 thousand = 8
