eb494693b45fd3617d917f3093acf08684e6df3e
himynameisfil <nguye208@gmail.com>
2015-04-05 12:45:37 -0400

    Update problem003.py

--- problem003.py
+++ problem003.py
@@ -1,12 +1,10 @@
-# Project-Euler-dot-com
-#Solutions to problems done on projecteuler.com
-#Largest prime factor
+#Status: Solved
 #Problem: The prime factors of 13195 are 5, 7, 13 and 29.
 #What is the largest prime factor of the number 600851475143 ?
 
 #Solution: This program will look for factors of our number and 
 #reduce our number every time a factor is found and start the process again.
-#This will continue until there are no more factors. aka the number left is prime. 
+#This will continue until there are no more factors. i.e. the number left is prime. 
 #Since it keeps reducing the number by dividing by factors from smallest to largest, the number remaining must be the largest
 n=600851475143
 i=2
