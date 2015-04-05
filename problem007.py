35f14bb9e3283f1660976a984eaa89c041ca3e5d
himynameisfil <nguye208@gmail.com>
2015-04-05 12:48:31 -0400

    Update problem007.py

--- problem007.py
+++ problem007.py
@@ -1,5 +1,5 @@
-# Project-Euler-dot-com
-#Solutions to problems done on projecteuler.com
+#Status: Solved
+
 #Problem : By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10 001st prime number?
 
