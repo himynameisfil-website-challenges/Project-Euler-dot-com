0afc5ded3ed9beb34b9f91e4fd1014e407f7ea33
himynameisfil <nguye208@gmail.com>
2015-04-05 12:51:32 -0400

    Update problem009.py

--- problem009.py
+++ problem009.py
@@ -1,10 +1,12 @@
-# Project-Euler-dot-com
-#Solutions to problems done on projecteuler.com
+#Status: Solved
+
 #Problem: A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 #a2 + b2 = c2
 #For example, 32 + 42 = 9 + 16 = 25 = 52.
 #There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 #Find the product abc.
+
+#Solution:
 a=0
 b=0
 c=0
