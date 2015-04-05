137ff4a9bf53e01d76dfc28910f4e2a362291631
himynameisfil <nguye208@gmail.com>
2015-04-05 13:40:43 -0400

    Update problem018.py

--- problem018.py
+++ problem018.py
@@ -1,5 +1,12 @@
-# Project-Euler-dot-com
-Solutions to problems done on projecteuler.com
+#Status: Solved
+#Problem: By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+#3
+#7 4
+#2 4 6
+#8 5 9 3
+#That is, 3 + 7 + 4 + 9 = 23.
+#Find the maximum total from top to bottom of the triangle below:
+
 n = """75
 95 64
 17 47 82
