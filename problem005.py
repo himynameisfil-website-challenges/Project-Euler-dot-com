d8ae60aaf5ecf0decff9f17f813c386de74bbb79
himynameisfil <nguye208@gmail.com>
2015-04-05 12:47:20 -0400

    Update problem005.py

--- problem005.py
+++ problem005.py
@@ -1,6 +1,4 @@
-# Project-Euler-dot-com
-#Solutions to problems done on projecteuler.com
-#smallest multiple
+#Status: Solved
 #Problem: 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 #What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
