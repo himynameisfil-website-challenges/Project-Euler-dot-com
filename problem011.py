26e49f1b9570cefff72699ce4e192e032e356c88
himynameisfil <nguye208@gmail.com>
2015-04-05 12:55:02 -0400

    Update problem011.py

--- problem011.py
+++ problem011.py
@@ -1,5 +1,12 @@
-# Project-Euler-dot-com
-Solutions to problems done on projecteuler.com
+##Status: Solved
+##
+##Problem:In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+##Number listed below: source https://projecteuler.net/problem=11
+##The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+##
+##What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+
+
 n="""08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
 49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
 81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
@@ -58,3 +65,5 @@ for i in range(0,20):
         if current>largest:
             largest=current
 print(largest)
+
+
